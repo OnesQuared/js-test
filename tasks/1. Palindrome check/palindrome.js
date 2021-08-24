edffcdca5675d0d55776b14f2298e4b38c6e00bc
OnesQuared <bakamono.moon@gmail.com>
2021-08-24 16:54:29 -0400

    palindrome
    palindrome function code

--- tasks/1. Palindrome check/palindrome.js
+++ tasks/1. Palindrome check/palindrome.js
@@ -1,3 +1,8 @@
 export function isPalindrome(inputString) {
-  // TODO: write your code here
+   var length = Math.floor(inputString.length / 2);
+
+   for (var i = 0; i < length; i++)
+    if (inputString[i] !== inputString[inputString.length - i - 1])
+      return false;
+  return true;
 }
